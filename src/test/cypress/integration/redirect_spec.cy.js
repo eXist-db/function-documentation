dd04c72cdf6fdf24ef5c63c4f0b461a054cf9ea4
Juri Leino <github@line-o.de>
2025-09-18 15:32:23 +0200

    test: improve redirect testsuite

--- src/test/cypress/integration/redirect_spec.cy.js
+++ src/test/cypress/integration/redirect_spec.cy.js
@@ -3,15 +3,26 @@
 
 context('Visiting a legacy URL', () => {
     it('redirects to the new index route', () => {
-      cy.visit('index.html?action=search&type=name&q=tei')
-      cy.url().should('equal', 'http://localhost:8080/exist/apps/fundocs/?action=search&where=everywhere&q=tei')
+      cy.visit('index.html', { qs: { action: 'search', type: 'name', q: 'tei'}})
+
+      cy.location('pathname').should('eq', '/exist/apps/fundocs/')
+      cy.location('search').should('include', 'action=search')
+      cy.location('search').should('include', 'where=everywhere')
+      cy.location('search').should('include', 'q=tei')
     })
     it('redirects to the new view route', () => {
-      cy.visit('view.html?uri=http://exist-db.org/xquery/file&function=file:sync&arity=3&location=java:org.exist.xquery.modules.file.FileModule')
-      cy.url().should('equal', 'http://localhost:8080/exist/apps/fundocs/view?location=java%3Aorg.exist.xquery.modules.file.FileModule&uri=http%3A%2F%2Fexist-db.org%2Fxquery%2Ffile&function=file%3Async')
+      cy.visit('view.html', { qs: { uri: 'http://exist-db.org/xquery/file', function: 'file:sync', location: 'java:org.exist.xquery.modules.file.FileModule'}})
+
+      cy.location('pathname').should('eq', '/exist/apps/fundocs/view')
+      cy.location('search').should('include', 'uri=http%3A%2F%2Fexist-db.org%2Fxquery%2Ffile')
+      cy.location('search').should('include', 'function=file%3Async')
+      cy.location('search').should('include', 'location=java%3Aorg.exist.xquery.modules.file.FileModule')
     })
     it('redirects to the new browse URL', () => {
-      cy.visit('browse.html?w3c=true&appmodules=true')
-      cy.url().should('equal', 'http://localhost:8080/exist/apps/fundocs/browse?w3c=true&appmodules=true')
+      cy.visit('browse.html', { qs: { w3c: 'true', appmodules: 'true'}})
+
+      cy.location('pathname').should('eq', '/exist/apps/fundocs/browse')
+      cy.location('search').should('include', 'w3c=true')
+      cy.location('search').should('include', 'appmodules=true')
     })
 })
